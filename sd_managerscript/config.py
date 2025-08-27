7d2e29bb848e4060c4f5e7ad796df02dcd6cdc3a
mb <mb@magenta-aps.dk>
2025-08-27 11:52:07 +0200

    feat: [#66073] rewrite manager sync to FastRAMQPI

--- sd_managerscript/config.py
+++ sd_managerscript/config.py
@@ -1,31 +1,16 @@
-# SPDX-FileCopyrightText: 2022 Magenta ApS <https://magenta.dk>
+# SPDX-FileCopyrightText: Magenta ApS <https://magenta.dk>
 # SPDX-License-Identifier: MPL-2.0
-from functools import cache
-from typing import Any
 from uuid import UUID
 
-from pydantic import AnyHttpUrl
+from fastramqpi.config import Settings as FastRAMQPISettings
 from pydantic import BaseSettings
 from pydantic import Field
-from pydantic import parse_obj_as
-from pydantic import SecretStr
-
-from .init import ManagerLevel
 
 
-class Settings(BaseSettings):
-    graphql_timeout: int = 120
-    client_id: str = Field("SD-Managerscript", description="Client ID for OIDC client.")
-    client_secret: SecretStr = Field(..., description="Client Secret for OIDC client.")
-    mo_url: AnyHttpUrl = Field(
-        parse_obj_as(AnyHttpUrl, "http://mo-service:5000"),
-        description="Base URL for OS2mo.",
+class ManagerSyncSettings(BaseSettings):
+    fastramqpi: FastRAMQPISettings = Field(
+        default_factory=FastRAMQPISettings, description="FastRAMQPI settings"
     )
-    auth_server: AnyHttpUrl = Field(
-        parse_obj_as(AnyHttpUrl, "http://keycloak-service:8080/auth"),
-        description="Base URL for OIDC server (Keycloak).",
-    )
-    auth_realm: str = Field("mo", description="Realm to authenticate against")
     root_uuid: UUID = Field(description="UUID of the root org-unit")
     manager_type_uuid: UUID = Field(
         description="UUID defining manager type. Same for all managers"
@@ -36,20 +21,7 @@ class Settings(BaseSettings):
     manager_level_mapping: dict[str, str] = Field(
         description="Mapping dict from org-unit level to manager level"
     )
-    manager_level_create: list[ManagerLevel]
-
-    log_level: str = "INFO"
 
 
-@cache
-def get_settings(*args: Any, **kwargs: Any) -> Settings:
-    """Fetch settings object.
-
-    Args:
-        args: overrides
-        kwargs: overrides
-
-    Return:
-        Cached settings object.
-    """
-    return Settings(*args, **kwargs)
+def get_settings(*args, **kwargs) -> ManagerSyncSettings:
+    return ManagerSyncSettings(*args, **kwargs)
